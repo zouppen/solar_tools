ae1b79dc509a27d6d9c6597a0630734fa00869ff
Joel Lehtonen <joel.lehtonen@iki.fi>
2024-05-16 23:23:11 +0300

    Aggressive refactor to get config non-hardcoded

--- IntegrateEnergy.hs
+++ IntegrateEnergy.hs
@@ -1,11 +1,16 @@
 {-# LANGUAGE OverloadedStrings, BangPatterns, RecordWildCards #-}
 
+import System.Environment (getArgs)
+
+import qualified Data.Yaml as Y
+import Data.Aeson
 import Data.ByteString (ByteString)
 import Database.PostgreSQL.Simple
 import Data.Scientific (Scientific)
 import Data.Int (Int32, Int64)
 import Text.Read (readMaybe)
 import Control.Monad (void)
+import Data.String (IsString, fromString)
 
 data State = State { epoch       :: Scientific
                    , jouleSum    :: Scientific
@@ -21,15 +26,18 @@ data Config = Config { connString :: ByteString
                      , initialGet :: Query
                      , select     :: Query
                      , insert     :: Query
-                     }
+                     } deriving (Show)
 
-config = Config { connString = "dbname=sensor"
-                , stateGet = "select cursor from cursor where source='joule_state'"
-                , stateSet = "insert into cursor values ('joule_state', ?) on conflict (source) do update set cursor=excluded.cursor"
-                , initialGet = "select extract(epoch from ts) from aurinko order by ts limit 1"
-                , select =  "select id, extract(epoch from ts), solar_power from aurinko where ts>to_timestamp(?) order by ts"
-                , insert = "insert into aurinko_joule (id, joule_cum) values (?, ?)"
-                }
+instance FromJSON Config where
+  parseJSON = withObject "Config" $ \v ->
+    let str k = fromString <$> v .: k
+    in Config
+       <$> str "connString"
+       <*> str "stateGet"
+       <*> str "stateSet"
+       <*> str "initialGet"
+       <*> str "select"
+       <*> str "insert"
 
 -- |Helper to handle getting initial values, containing only one single answer row
 singleQuery :: FromRow a => Connection -> IO b -> (a -> IO b) -> Query -> IO b
@@ -41,37 +49,38 @@ singleQuery conn whenNone whenOne q = do
     _   -> fail $ "More than one answer to this query: " ++ show q
 
 -- |Load state from db or generate an initial one
-stateInit :: Connection -> IO State
-stateInit conn = do
+stateInit :: Config -> Connection -> IO State
+stateInit Config{..} conn = do
   let none = do
         let none = fail "No data in 'aurinko' table"
             one [e] = pure $ State e 0
-          in singleQuery conn none one $ initialGet config
+          in singleQuery conn none one initialGet
       one [a] = maybe stateFail pure (readMaybe a)
-    in singleQuery conn none one $ stateGet config
+    in singleQuery conn none one stateGet
   where stateFail = fail "Invalid state format, consider dropping state, \
                          \truncating table and repopulating everything"
 
 -- |Stores the state to the database
-storeState :: Connection -> State -> IO ()
-storeState conn st = void $ execute conn (stateSet config) [show st]
+storeState :: Config -> Connection -> State -> IO ()
+storeState Config{..} conn st = void $ execute conn stateSet [show st]
 
 -- |Integrate unprocessed data from database and folding it
-integrate :: Connection -> State -> IO (State, Stats)
-integrate conn st = fold conn (select config)
-  [epoch st] (st, Stats 0 0) (integrator conn)
+integrate :: Config -> Connection -> State -> IO (State, Stats)
+integrate conf@Config{..} conn st = fold conn select
+  [epoch st] (st, Stats 0 0) (integrator conf conn)
 
 -- |Folding function which handles single input row.
 integrator
-  :: Connection
+  :: Config
+  -> Connection
   -> (State, Stats)
   -> (Int32, Scientific, Scientific)
   -> IO (State, Stats)
-integrator conn (State oldTime oldSum, Stats{..}) (id, newTime, power) = do
+integrator Config{..} conn (State oldTime oldSum, Stats{..}) (id, newTime, power) = do
   -- Inserting data. Do not insert if it didn't increment
   if round oldSum == newRounded
     then pure $ (State newTime newSum, Stats added (skipped + 1))
-    else do execute conn (insert config) (id, newRounded)
+    else do execute conn insert (id, newRounded)
             pure (State newTime newSum, Stats (added + 1) skipped)
   where delta = newTime - oldTime
         joules = power * delta
@@ -80,9 +89,17 @@ integrator conn (State oldTime oldSum, Stats{..}) (id, newTime, power) = do
 
 main :: IO ()
 main = do
-  conn <- connectPostgreSQL $ connString config
+  args <- getArgs
+  conf <- case args of
+    [confPath] -> do
+      parsed <- Y.decodeFileEither confPath
+      case parsed of
+        Left e -> fail $ "Parse error in configuration file: " ++ show e
+        Right a -> pure a
+    _ -> fail $ "Give configuration file as the only argument"
+  conn <- connectPostgreSQL $ connString conf
   withTransaction conn $ do
-    state <- stateInit conn
-    (newState, stats) <- integrate conn state
-    storeState conn newState
+    state <- stateInit conf conn
+    (newState, stats) <- integrate conf conn state
+    storeState conf conn newState
     putStrLn $ "Finished. " ++ show stats
