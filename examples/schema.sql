e5696a7bee44a79bc0be6695e4a91b0e1aaa685d
Joel Lehtonen <joel.lehtonen@iki.fi>
2024-10-04 03:21:59 +0300

    Update procedure which suppresses successive duplicates

--- examples/schema.sql
+++ examples/schema.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE insert_distinct_event(new_name character(16), new_value jsonb)
+CREATE OR REPLACE PROCEDURE insert_distinct_event(new_site smallint, new_name varchar(16), new_value jsonb)
 LANGUAGE plpgsql AS $$
 DECLARE
     allow_insert BOOLEAN;
@@ -6,14 +6,19 @@ BEGIN
     -- Testaa, onko viimeisin arvo eri kuin uusi
     SELECT value <> new_value
     INTO allow_insert
-    FROM event
-    WHERE name = new_name
+    FROM misc_event
+    WHERE new_site = site AND name = new_name
     ORDER BY ts DESC;
 
     -- Lisää rivi, jos arvo on uusi (tai ei aiempia rivejä)
     IF COALESCE(allow_insert, TRUE) THEN
-        INSERT INTO event (name, value)
-        VALUES (new_name, new_value);
+        INSERT INTO misc_event (site, name, value)
+        VALUES (new_site, new_name, new_value);
     END IF;
 END;
 $$;
+
+-- Migration from old version where target used to be text
+UPDATE misc_event
+  SET value=COALESCE(jsonb_set(value, '{"target"}', to_jsonb(substring(value->>'explanation' from '^Target (.*)%$')::numeric)), value)
+  WHERE name='decision' AND site=1 AND value->'target' IS NULL;
