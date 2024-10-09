4434899712860cb02647ef6f7a54976edf4b627f
Joel Lehtonen <joel.lehtonen@iki.fi>
2024-10-09 13:51:21 +0300

    Split Binner from its main function

---
+++ Binner/Binner.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE OverloadedStrings, RecordWildCards, DeriveGeneric, DeriveAnyClass #-}
+module Binner.Binner where
+
+import Control.Exception (Exception)
+import Control.Monad (void, when)
+import Control.Monad.Extra (whileM)
+import Data.Aeson
+import Data.ByteString (ByteString)
+import Data.Scientific
+import qualified Data.Yaml as Y
+import Database.PostgreSQL.Simple
+import GHC.Generics
+
+import Common.Timer
+import Common.Binning
+import Common.DbHelpers
+import Common.ConfigHelpers
+
+data Config = Config
+  { connString :: ByteString       -- ^PostgreSQL connection string
+  , before     :: Maybe Query      -- ^SQL to run before operation
+  , select     :: Query            -- ^SQL which returns: id, prev, current
+  , update     :: Query            -- ^SQL which takes: id, bin
+  , debug      :: Maybe Bool       -- ^Do debug printing (default: off)
+  , txInterval :: Maybe Scientific -- ^Commit in a safe point every
+                                   -- this seconds. (default: commit
+                                   -- in the end)
+  } deriving (Generic, Show)
+
+instance FromJSON Config where
+  parseJSON = genericParseJSON opts{fieldLabelModifier = fieldMangler 0}
+
+data InputRow = InputRow
+  { eventId    :: Integer
+  , mbPrevTime :: Maybe Scientific
+  , curTime    :: Scientific
+  } deriving (Generic, FromRow, Show)
+
+data BinResult = BinResult { binned  :: !Int
+                           , skipped :: !Int
+                           } deriving (Show)
+
+instance Exception BinResult
+
+runBinner :: FilePath -> SharedConnection -> IO ()
+runBinner confFile sharedDb = do
+  config@Config{..} <- Y.decodeFileThrow confFile
+  let dbg = when (debug == Just True)
+  -- Connect to database and run preparatory SQL
+  conn <- connectSharedDb sharedDb connString
+  whenJust_ before $ execute_ conn
+  -- Timer which allows us to do it incrementally
+  timer <- case txInterval of
+      Nothing -> newInfiniteTimer
+      Just t  -> newTimer t
+  whileM $ withTransaction conn $ do
+    atomically $ startTimer timer
+    let f = fold_ conn select (BinResult 0 0)
+    (timeout, stats) <- withTimeout timer f $ \BinResult{..} InputRow{..} -> do
+      case mbPrevTime of
+        Nothing -> do
+          dbg $ putStrLn $ "Skipping first event, id=" <> show eventId
+          pure BinResult{skipped = skipped+1, ..}
+        Just prevTime -> do
+          execute conn update (eventId, toBin prevTime curTime)
+          pure BinResult{binned = binned+1, ..}
+    -- Show signs of life
+    let extra = if timeout then " Continuing..." else ""
+    putStrLn $ show stats <> extra
+    -- Keep on running if needed
+    pure timeout
